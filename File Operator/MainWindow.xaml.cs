9e599aca8180cdf3da29bd3634c014242b33d59a
Ivan Stoychev <ivanstoychev44@gmail.com>
2018-06-06 16:23:19 +0300

    Base design skeleton.

--- File Operator/MainWindow.xaml.cs
+++ File Operator/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace File_Operator
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
