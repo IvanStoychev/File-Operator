12cfa5f7b8bf8cbd4e1697b2ff11db7b79b95f73
Ivan Stoychev <Ivan.Stoychev@isg-technology.com>
2021-06-27 01:25:21 +0300

    Basic design skeleton and ViewModel implemented.
    Added a FolderBrowserDialog functionality.

---
+++ File-Operator/Frontend/ViewModels/FolderCompareViewModel.cs
@@ -0,0 +1,78 @@
+﻿using Frontend.Utility;
+using System.ComponentModel;
+using System.Windows.Forms;
+
+namespace Frontend.ViewModels
+{
+    public class FolderCompareViewModel : INotifyPropertyChanged
+    {
+        string sourceFolderPath;
+        string targetFolderPath;
+
+        public string SourceFolderPath
+        {
+            get => sourceFolderPath;
+            set
+            {
+                if (sourceFolderPath != value)
+                {
+                    sourceFolderPath = value;
+                    OnPropertyChanged("SourceFolderPath");
+                }
+            }
+        }
+
+        public string TargetFolderPath
+        {
+            get => targetFolderPath;
+            set
+            {
+                if (targetFolderPath != value)
+                {
+                    targetFolderPath = value;
+                    OnPropertyChanged("TargetFolderPath");
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string propertyName) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        public RelayCommand BrowseSourceFolderCommand { get; }
+        public RelayCommand BrowseTargetFolderCommand { get; }
+        public RelayCommand CompareFoldersCommand { get; }
+
+        public FolderCompareViewModel()
+        {
+            BrowseSourceFolderCommand = new RelayCommand(OnBrowseSourceFolderCommand);
+            BrowseTargetFolderCommand = new RelayCommand(OnBrowseTargetFolderCommand);
+            CompareFoldersCommand = new RelayCommand(OnCompareFoldersCommand, CanCompareFoldersCommand);
+        }
+
+        void OnBrowseSourceFolderCommand()
+        {
+            FolderBrowserDialog sourceFolderBrowser = new();
+            sourceFolderBrowser.Description = "desc";
+            sourceFolderBrowser.ShowNewFolderButton = false;
+            if (sourceFolderBrowser.ShowDialog() == DialogResult.OK)
+            {
+                SourceFolderPath = sourceFolderBrowser.SelectedPath;
+            }
+        }
+
+        void OnBrowseTargetFolderCommand()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        void OnCompareFoldersCommand()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        bool CanCompareFoldersCommand()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
